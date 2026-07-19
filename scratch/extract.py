503213f49de0b01df9033e8b205f81978e33fb61
Aswin N <kit28.24bam009@gmail.com>
2026-07-19 20:43:16 +0530

    commit old folder structure

--- scratch/extract.py
+++
@@ -1,5 +0,0 @@
-import re
-data = open('static/chatbot/assets/index.js', encoding='utf-8').read()
-idx = data.find("I couldn't play audio right now")
-with open('scratch/out.txt', 'w', encoding='utf-8') as f:
-    f.write(data[max(0, idx-1000):idx+1000])
--- scratch/extract2.py
+++
@@ -1,7 +0,0 @@
-import re
-data = open('static/chatbot/assets/index.js', encoding='utf-8').read()
-idx = data.find('className:`sidebar')
-if idx == -1:
-    idx = data.find('className:"sidebar')
-with open('scratch/sidebar.txt', 'w', encoding='utf-8') as f:
-    f.write(data[max(0, idx-100):idx+800])
